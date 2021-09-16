e55998504d2216c6e07d6bcf5a036b76bfe110bd
vbertazzo <vbertazzo@gmail.com>
2021-09-16 15:13:45 -0300

    Move files logic to its own hook
    Signed-off-by: vbertazzo <vbertazzo@gmail.com>

--- src/app.tsx
+++ src/app.tsx
@@ -1,110 +1,20 @@
-import { useRef, useState, MouseEvent, useEffect } from 'react'
-import { v4 as uuid } from 'uuid'
 import styled from 'styled-components/macro'
 
-import { File } from 'resources/files/types'
 import { Sidebar } from 'sidebar'
 import { Content } from 'content'
 
+import { useFiles } from 'resources/files/use-files'
+
 export function App () {
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [files, setFiles] = useState<File[]>([])
-
-  const selectedFile = files.find(file => file.active)
-
-  useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>
-
-    function updateStatus () {
-      if (!selectedFile || selectedFile.status !== 'editing') {
-        return
-      }
-
-      timer = setTimeout(() => {
-        setFiles(prevState => prevState.map(file => {
-          if (file.id === selectedFile.id) {
-            return {
-              ...file,
-              status: 'saving',
-            }
-          }
-
-          return file
-        }))
-
-        setTimeout(() => {
-          setFiles(prevState => prevState.map(file => {
-            if (file.id === selectedFile.id) {
-              return {
-                ...file,
-                status: 'saved',
-              }
-            }
-
-            return file
-          }))
-        }, 300)
-      }, 300)
-    }
-
-    updateStatus()
-
-    return () => clearTimeout(timer)
-  }, [selectedFile])
-
-  const handleCreateFile = () => {
-    inputRef.current?.focus()
-
-    setFiles(prevState => prevState
-      .map(file => ({
-        ...file,
-        active: false,
-      }))
-      .concat({
-        id: uuid(),
-        name: 'Sem título',
-        content: '',
-        active: true,
-        status: 'saved',
-      }))
-  }
-
-  const handleRemoveFile = (id: string) => {
-    setFiles(prevState => prevState.filter(file => file.id !== id))
-  }
-
-  const handleSelectFile = (id: string) => (e: MouseEvent) => {
-    e.preventDefault()
-    inputRef.current?.focus()
-
-    setFiles(prevState => prevState.map(file => ({
-      ...file,
-      active: file.id === id,
-    })))
-  }
-
-  const handleUpdateFile = ({ type, value }: Record<string, string>) => {
-    if (!selectedFile) {
-      return
-    }
-
-    const validProps = ['name', 'content']
-
-    if (!validProps.includes(type)) {
-      return
-    }
-
-    setFiles(prevState => prevState.map(file => {
-      if (file.id === selectedFile.id) {
-        return {
-          ...file,
-          [type]: value,
-          status: 'editing',
-        }
-      }
-      return file
-    }))
-  }
+  const {
+    files,
+    inputRef,
+    selectedFile,
+    handleCreateFile,
+    handleSelectFile,
+    handleRemoveFile,
+    handleUpdateFile,
+  } = useFiles()
 
   return (
     <Container>
---
+++ src/resources/files/use-files.ts
@@ -0,0 +1,114 @@
+import { useRef, useState, MouseEvent, useEffect } from 'react'
+import { v4 as uuid } from 'uuid'
+import { File } from 'resources/files/types'
+
+export function useFiles () {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [files, setFiles] = useState<File[]>([])
+
+  const selectedFile = files.find(file => file.active)
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>
+
+    function updateStatus () {
+      if (!selectedFile || selectedFile.status !== 'editing') {
+        return
+      }
+
+      timer = setTimeout(() => {
+        setFiles(prevState => prevState.map(file => {
+          if (file.id === selectedFile.id) {
+            return {
+              ...file,
+              status: 'saving',
+            }
+          }
+
+          return file
+        }))
+
+        setTimeout(() => {
+          setFiles(prevState => prevState.map(file => {
+            if (file.id === selectedFile.id) {
+              return {
+                ...file,
+                status: 'saved',
+              }
+            }
+
+            return file
+          }))
+        }, 300)
+      }, 300)
+    }
+
+    updateStatus()
+
+    return () => clearTimeout(timer)
+  }, [selectedFile])
+
+  const handleCreateFile = () => {
+    inputRef.current?.focus()
+
+    setFiles(prevState => prevState
+      .map(file => ({
+        ...file,
+        active: false,
+      }))
+      .concat({
+        id: uuid(),
+        name: 'Sem título',
+        content: '',
+        active: true,
+        status: 'saved',
+      }))
+  }
+
+  const handleRemoveFile = (id: string) => {
+    setFiles(prevState => prevState.filter(file => file.id !== id))
+  }
+
+  const handleSelectFile = (id: string) => (e: MouseEvent) => {
+    e.preventDefault()
+    inputRef.current?.focus()
+
+    setFiles(prevState => prevState.map(file => ({
+      ...file,
+      active: file.id === id,
+    })))
+  }
+
+  const handleUpdateFile = ({ type, value }: Record<string, string>) => {
+    if (!selectedFile) {
+      return
+    }
+
+    const validProps = ['name', 'content']
+
+    if (!validProps.includes(type)) {
+      return
+    }
+
+    setFiles(prevState => prevState.map(file => {
+      if (file.id === selectedFile.id) {
+        return {
+          ...file,
+          [type]: value,
+          status: 'editing',
+        }
+      }
+      return file
+    }))
+  }
+
+  return {
+    files,
+    inputRef,
+    handleCreateFile,
+    handleRemoveFile,
+    handleSelectFile,
+    handleUpdateFile,
+    selectedFile,
+  }
+}
